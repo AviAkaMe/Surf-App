973d1ecf87cd8d3c3c3c4500fbf3a840a848ba9c
AviAkaMe <avshalomt83@gmail.com>
2023-08-27 13:22:34 +0300

    start

--- lib/pages/wind_page.dart
+++ lib/pages/wind_page.dart
@@ -1,4 +1,5 @@
 // import 'package:flutter/material.dart';
+// import 'package:geolocator/geolocator.dart';
 //
 // class WindPage extends StatefulWidget {
 //   static const String id = 'wind_page';
@@ -9,6 +10,33 @@
 //
 // class _WindPageState extends State<WindPage> {
 //   String _selectedValue = '0:30';
+//
+//   Future<void> checkAndRequestLocationPermission() async {
+//     LocationPermission permission = await Geolocator.checkPermission();
+//
+//     if (permission == LocationPermission.denied) {
+//       permission = await Geolocator.requestPermission();
+//
+//       if (permission == LocationPermission.deniedForever) {
+//         // The user has permanently denied location permissions.
+//         // Handle this case gracefully, possibly by displaying a dialog.
+//       } else if (permission == LocationPermission.denied) {
+//         // The user denied location permissions.
+//         // You can display a message or request permissions again.
+//       } else {
+//         // Permission granted. You can proceed to get the user's location.
+//         Position position = await Geolocator.getCurrentPosition();
+//         // Now you can send this location to your cloud app.
+//       }
+//     } else if (permission == LocationPermission.deniedForever) {
+//       // The user has permanently denied location permissions.
+//       // Handle this case gracefully, possibly by displaying a dialog.
+//     } else {
+//       // Permission already granted. You can proceed to get the user's location.
+//       Position position = await Geolocator.getCurrentPosition();
+//       // Now you can send this location to your cloud app.
+//     }
+//   }
 //
 //   @override
 //   Widget build(BuildContext context) {
@@ -68,14 +96,14 @@
 //               ),
 //               SizedBox(height: 20),
 //               ElevatedButton(
-//                 onPressed: () {
-//                   // Add your logic here for the button press
-//                 },
 //                 style: ElevatedButton.styleFrom(
 //                   minimumSize:
 //                       Size(200, 60), // Set the minimum size of the button
 //                   // You can customize other properties here as well, such as background color, etc.
 //                 ),
+//                 onPressed: () async {
+//                   await checkAndRequestLocationPermission();
+//                 },
 //                 child: Text(
 //                   'Find The Wind',
 //                   style:
@@ -99,6 +127,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class WindPage extends StatefulWidget {
   static const String id = 'wind_page';
@@ -109,6 +139,7 @@ class WindPage extends StatefulWidget {
 
 class _WindPageState extends State<WindPage> {
   String _selectedValue = '0:30';
+  List<String> _helloLines = []; // Declare _helloLines here
 
   Future<void> checkAndRequestLocationPermission() async {
     LocationPermission permission = await Geolocator.checkPermission();
@@ -137,6 +168,22 @@ class _WindPageState extends State<WindPage> {
     }
   }
 
+  Future<void> fetchHelloLines() async {
+    final response = await http.get(Uri.parse(
+        'https://us-central1-b-surf.cloudfunctions.net/getHelloLines?value=$_selectedValue'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonResponse = json.decode(response.body);
+      final List<String> helloLines = jsonResponse.cast<String>();
+
+      setState(() {
+        _helloLines = helloLines;
+      });
+    } else {
+      // Handle error
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -149,74 +196,98 @@ class _WindPageState extends State<WindPage> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.wind_power,
-                size: 125,
-                color: Colors.black,
-              ),
-              SizedBox(height: 30),
-              Text(
-                'Please select Max Drive Time: ',
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 10),
-              DropdownButton<String>(
-                value: _selectedValue,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    _selectedValue = newValue!;
-                  });
-                },
-                items: <String>[
-                  '0:30',
-                  '1:00',
-                  '1:30',
-                  '2:00',
-                  '2:30',
-                  '3:00',
-                  '3:30',
-                  '4:00',
-                  '4:30',
-                  '5:00',
-                  '5:30',
-                  '6:00',
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize:
-                      Size(200, 60), // Set the minimum size of the button
-                  // You can customize other properties here as well, such as background color, etc.
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('lib/images/windy.jpg'),
+                    radius: 120,
+                  ),
                 ),
-                onPressed: () async {
-                  await checkAndRequestLocationPermission();
-                },
-                child: Text(
-                  'Find The Wind',
-                  style:
-                      TextStyle(fontSize: 20), // Adjust the font size if needed
+                SizedBox(height: 30),
+                Text(
+                  'Please select Max Drive Time: ',
+                  style: TextStyle(fontSize: 15),
+                  textAlign: TextAlign.center,
                 ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                width: 350,
-                height: 125,
-                color: Colors.deepOrange,
-              ),
-              SizedBox(height: 20),
-            ],
+                SizedBox(height: 10),
+                DropdownButton<String>(
+                  value: _selectedValue,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      _selectedValue = newValue!;
+                    });
+                  },
+                  items: <String>[
+                    '0:30',
+                    '1:00',
+                    '1:30',
+                    '2:00',
+                    '2:30',
+                    '3:00',
+                    '3:30',
+                    '4:00',
+                    '4:30',
+                    '5:00',
+                    '5:30',
+                    '6:00',
+                  ].map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize:
+                        Size(200, 60), // Set the minimum size of the button
+                    // You can customize other properties here as well, such as background color, etc.
+                  ),
+                  onPressed: () async {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                    );
+                    await checkAndRequestLocationPermission();
+                    await fetchHelloLines(); // Call the Firebase Function
+                    Navigator.pop(context); // Close the loading dialog
+                  },
+                  child: Text(
+                    'Find The Wind',
+                    style: TextStyle(
+                        fontSize: 20), // Adjust the font size if needed
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      border: Border.all(color: Colors.blueAccent)),
+                  child: Column(
+                    children: _helloLines
+                        .map(
+                          (line) => Text(
+                            line,
+                            style: TextStyle(fontSize: 16, color: Colors.black),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
--- surf_func/index.js
+++ surf_func/index.js
@@ -1,19 +1,16 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const functions = require('firebase-functions');
 const axios = require('axios');
 
 admin.initializeApp();
 const db = admin.firestore();
 const fcm = admin.messaging();
+
+exports.getHelloLines = functions.https.onRequest((request, response) => {
+  const value = request.query.value || '0:30'; // Default value if none provided
+  const lines = Array(15).fill(`Selected value: ${value}`);
+  response.send(lines);
+});
 
 exports.getStrongWinds = functions.https.onRequest(async (request, response) => {
     try {
@@ -62,7 +59,7 @@ exports.getStrongWinds = functions.https.onRequest(async (request, response) => 
     }
 });
 
-exports.scheduledAlerts = functions.pubsub.schedule('0 10 * * *')
+exports.scheduledAlerts = functions.pubsub.schedule('55 16 * * *')
     .timeZone('Asia/Jerusalem') // Israel timezone
     .onRun(async (context) => {
         try {
@@ -105,11 +102,3 @@ exports.scheduledAlerts = functions.pubsub.schedule('0 10 * * *')
             return null;
         }
     });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
