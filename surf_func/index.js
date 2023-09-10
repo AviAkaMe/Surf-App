88510d3c199d7204456f0e427bbf45f0b261244f
AviAkaMe <avshalomt83@gmail.com>
2023-09-10 17:25:56 +0300

    start

--- surf_func/index.js
+++ surf_func/index.js
@@ -6,6 +6,8 @@ admin.initializeApp();
 const db = admin.firestore();
 const fcm = admin.messaging();
 
+//////////////////////////////////////////////////////////////////////////
+
 exports.calculateTravelTimes = functions.https.onRequest(async (req, res) => {
   try {
     // Get user's origin coordinates from the request
@@ -23,44 +25,49 @@ exports.calculateTravelTimes = functions.https.onRequest(async (req, res) => {
       };
     });
 
-    // Prepare API request
-    const apiKey = 'AIzaSyC-Jb-ngFC5Pl-QRXJGAblhKVIQrSt0uRk';
-    const apiUrl = `https://maps.googleapis.com/maps/api/distancematrix/json`;
-    const params = {
-      origins: userCoords,
-      destinations: destinationData.map(item => item.coords).join('|'),
-      units: 'imperial',
+    console.log('Original Destination Locations:', JSON.stringify(destinationData));
+
+    // Modify the user's location to have a name and coords
+    const userLocation = {
+      name: 'currentUser', // You can choose any name you prefer, e.g., 'currentUser'
+      coords: userCoords,
+    };
+
+    // Add the user's location to the beginning of the locations array
+    const locations = [userLocation, ...destinationData];
+
+    console.log('Updated Destination Locations:', JSON.stringify(locations));
+
+    // Prepare API request for MapQuest Distance Matrix API
+    const apiKey = 'meUpZF1IJC1xerOyaagheDBFWvK5uOk1';
+    const apiUrl = 'https://www.mapquestapi.com/directions/v2/routematrix';
+
+    const requestBody = {
+      locations: locations.map(item => item.coords), // Use the modified locations array
+      options: {
+        allToAll: false, // Set to false to calculate travel times from one source to all destinations
+        unit: 'm', // You can specify the unit you want here (meters)
+      },
       key: apiKey
     };
 
-    // Make API request to Google Distance Matrix API
-    const response = await axios.get(apiUrl, { params });
+    // Make API request to MapQuest Distance Matrix API
+    const response = await axios.post(apiUrl, requestBody);
 
-    if (response.data.status === 'OK') {
-      const travelTimes = response.data.rows[0].elements.map((element, index) => {
-        if (element.status === 'OK') {
-          const durationText = element.duration.text;
-          const durationMinutes = calculateDurationMinutes(durationText);
-          return {
-            duration: durationText,
-            status: element.status,
-            name: destinationData[index].name, // Include the name in the response
-            durationMinutes: durationMinutes.toString() // Include duration in minutes
-          };
-        } else {
-          return {
-            duration: 'N/A',
-            status: element.status,
-            name: destinationData[index].name, // Include the name even for failed requests
-            durationMinutes: 'N/A' // No duration for failed requests
-          };
-        }
+    if (response.data.info.statuscode === 0) {
+      const travelTimes = response.data.time.map((duration, index) => {
+        const minutes = Math.round(duration / 60); // Convert duration to rounded minutes
+        return {
+          duration: duration,
+          name: locations[index].name, // Use the modified locations array
+          durationMinutes: minutes
+        };
       });
 
       // Send the extracted travel times with names and duration in minutes
       res.json({ travelTimes });
     } else {
-      console.error('Google API Error:', response.data.status);
+      console.error('MapQuest API Error:', response.data.info.statuscode);
       res.status(500).json({ error: 'An error occurred while fetching data from the API' });
     }
   } catch (error) {
@@ -68,107 +75,7 @@ exports.calculateTravelTimes = functions.https.onRequest(async (req, res) => {
     res.status(500).json({ error: 'An error occurred' });
   }
 });
-
-// Helper function to calculate duration in minutes from duration text (e.g., "1 hour 58 mins")
-function calculateDurationMinutes(durationText) {
-  const parts = durationText.split(' ');
-  let minutes = 0;
-  for (let i = 0; i < parts.length; i += 2) {
-    const value = parseInt(parts[i]);
-    const unit = parts[i + 1];
-    if (unit === 'hour' || unit === 'hours') {
-      minutes += value * 60;
-    } else if (unit === 'min' || unit === 'mins') {
-      minutes += value;
-    }
-  }
-  return minutes;
-}
-
-//exports.calculateTravelTimes = functions.https.onRequest(async (req, res) => {
-//    try {
-//        // Get user's origin coordinates from the request
-//        const { lat, lng } = req.query;
-//        const userCoords = `${lat},${lng}`;
-//
-//        // Fetch destination locations from Firestore
-//        const locationsSnapshot = await admin.firestore().collection('locations').get();
-//        const destinationCoords = locationsSnapshot.docs.map(doc => {
-//            const geopoint = doc.data().cords;
-//            return `${geopoint.latitude},${geopoint.longitude}`;
-//        });
-//
-//        // Prepare API request
-//        const apiKey = 'AIzaSyC-Jb-ngFC5Pl-QRXJGAblhKVIQrSt0uRk';
-//        const apiUrl = `https://maps.googleapis.com/maps/api/distancematrix/json`;
-//        const params = {
-//            origins: userCoords,
-//            destinations: destinationCoords.join('|'),
-//            units: 'imperial',
-//            key: apiKey
-//        };
-//
-//        // Construct the URL for debugging purposes
-//        const constructedUrl = `${apiUrl}?${new URLSearchParams(params).toString()}`;
-//
-//        // Make API request to Google Distance Matrix API
-//        const response = await axios.get(apiUrl, { params });
-//
-//        if (response.data.status === 'OK') {
-//            const travelTimes = response.data.rows[0].elements.map(element => {
-//                if (element.status === 'OK') {
-//                    return {
-//                        duration: element.duration.text,
-//                        status: element.status
-//                    };
-//                } else {
-//                    return {
-//                        duration: 'N/A',
-//                        status: element.status
-//                    };
-//                }
-//            });
-//
-//            // Send the extracted travel times and constructed URL
-//            res.json({ travelTimes, constructedUrl });
-//        } else {
-//            console.error('Google API Error:', response.data.status);
-//            res.status(500).json({ error: 'An error occurred while fetching data from the API' });
-//        }
-//    } catch (error) {
-//        console.error('Error:', error);
-//        res.status(500).json({ error: 'An error occurred' });
-//    }
-//});
-
-//exports.findSpotsWind = functions.https.onRequest(async (request, response) => {
-//
-//  try {
-//    const value = request.query.value || '0:30';
-//    const latitude = request.query.latitude || 'unknown';
-//    const longitude = request.query.longitude || 'unknown';
-//
-//    const strongWindsResponse = await axios.get('https://us-central1-b-surf.cloudfunctions.net/getStrongWinds');
-//    const spotsWithStrongWindsArray = strongWindsResponse.data.spotsWithStrongWinds;
-//
-//    const lines = [];
-//
-//    spotsWithStrongWindsArray.forEach(spot => {
-//      const location = spot.name;
-//
-//      const windStartTime = new Date(spot.windStartTime);
-//      windStartTime.setHours(windStartTime.getHours() + 3); // Adding 3 hours
-//      const formattedWindStartTime = windStartTime.toISOString().substr(11, 5);
-//
-//      lines.push(`Wind starts today at ${location} at ${formattedWindStartTime}`);
-//    });
-//
-//    response.send(JSON.stringify(lines));
-//  } catch (error) {
-//    console.error('Error:', error);
-//    response.status(500).send('An error occurred');
-//  }
-//});
+///////////////////////////////////////////////////////////////////////////
 
 exports.findSpotsWind = functions.https.onRequest(async (request, response) => {
     try {
@@ -225,34 +132,17 @@ async function getStrongWindsData(date, dayLabel, value, latitude, longitude) {
     return lines;
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
 
 exports.getStrongWinds = functions.https.onRequest(async (request, response) => {
-    let targetTimeMinutes; // Initialize the variable to store minutes
     try {
-        const { date, value, latitude, longitude } = request.query;
+        const { date } = request.query;
         const locationsRef = db.collection('locations');
         const locationsSnapshot = await locationsRef.get();
-
-        let travelTimes = {};
-                if (value && latitude && longitude) {
-                    const travelTimesResponse = await axios.get(`https://us-central1-b-surf.cloudfunctions.net/calculateTravelTimes?lat=${latitude}&lng=${longitude}`);
-                    travelTimes = travelTimesResponse.data.travelTimes.reduce((acc, spot) => {
-                        acc[spot.name] = spot.durationMinutes;
-                        return acc;
-                    }, {});
-                }
 
         const spotsWithStrongWinds = {};
 
         const promises = locationsSnapshot.docs.map(async (doc) => {
-
-            if (value) {
-                const [hours, minutes] = value.split(':').map(Number); // Parse hours and minutes
-                targetTimeMinutes = hours * 60 + minutes; // Calculate total minutes
-            }
-
-            console.log('targetTimeMinutes:', targetTimeMinutes); // Print the value for testing
 
             const locationData = doc.data();
             const coords = locationData.cords;
@@ -292,16 +182,17 @@ exports.getStrongWinds = functions.https.onRequest(async (request, response) => 
         });
 
         await Promise.all(promises);
-             let spotsWithStrongWindsArray = Object.keys(spotsWithStrongWinds).map(name => ({
-                       name,
-                       windStartTime: spotsWithStrongWinds[name].toISOString(),
-                       durationMinutes: travelTimes[name] || null
-                   }));
+
+        const spotsWithStrongWindsArray = Object.keys(spotsWithStrongWinds).map(name => ({
+            name,
+            windStartTime: spotsWithStrongWinds[name].toISOString()
+        }));
+
+        console.log('Spots with strong winds:', spotsWithStrongWindsArray);
 
         const finalResponse = {
-                    spotsWithStrongWinds: spotsWithStrongWindsArray,
-                    travelTimes: travelTimes  // Include travel times in the response
-                };
+            spotsWithStrongWinds: spotsWithStrongWindsArray,
+        };
 
         response.status(200).json(finalResponse);
     } catch (error) {
@@ -310,91 +201,10 @@ exports.getStrongWinds = functions.https.onRequest(async (request, response) => 
     }
 });
 
-//exports.getStrongWinds = functions.https.onRequest(async (request, response) => {
-//    try {
-//        const { date, value, latitude, longitude } = request.query;
-//        const locationsRef = db.collection('locations');
-//        const locationsSnapshot = await locationsRef.get();
-//
-//        const spotsWithStrongWinds = {};
-//
-//        const promises = locationsSnapshot.docs.map(async (doc) => {
-//
-//            let targetTimeMinutes; // Initialize the variable to store minutes
-//            if (value) {
-//                const [hours, minutes] = value.split(':').map(Number); // Parse hours and minutes
-//                targetTimeMinutes = hours * 60 + minutes; // Calculate total minutes
-//            }
-//            console.log('targetTimeMinutes:', targetTimeMinutes); // Print the value for testing
-//
-//            const locationData = doc.data();
-//            const coords = locationData.cords;
-//
-//            const latitude = parseFloat(coords.latitude);
-//            const longitude = parseFloat(coords.longitude);
-//
-//            const currentDate = new Date();
-//            const formattedDate = date || currentDate.toISOString().substr(0, 10);
-//
-//            const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=windspeed_10m&current_weather=true&windspeed_unit=kn&start_date=${formattedDate}&end_date=${formattedDate}`);
-//
-//            const hourlyData = response.data.hourly;
-//
-//            let relevantHourlyData;
-//            let relevantWindspeedData;
-//
-//            if (formattedDate === currentDate.toISOString().substr(0, 10)) {
-//                const currentHour = currentDate.getUTCHours();
-//                const startIndex = Math.max(currentHour, 5);
-//                const endIndex = Math.max(16 - startIndex, 0);
-//                relevantHourlyData = hourlyData.time.slice(startIndex, startIndex + endIndex);
-//                relevantWindspeedData = hourlyData.windspeed_10m.slice(startIndex, startIndex + endIndex);
-//            } else {
-//                relevantHourlyData = hourlyData.time.slice(5, 5 + 11);  // 5:00-15:00utc
-//                relevantWindspeedData = hourlyData.windspeed_10m.slice(5, 5 + 11);
-//            }
-//
-//            const earliestIndex = relevantWindspeedData.findIndex(speed => speed >= 11); // speed in knots
-//
-//            if (earliestIndex !== -1) {
-//                const earliestTime = new Date(relevantHourlyData[earliestIndex]);
-//                if (!spotsWithStrongWinds[locationData.name] || earliestTime < spotsWithStrongWinds[locationData.name]) {
-//                    spotsWithStrongWinds[locationData.name] = earliestTime;
-//                }
-//            }
-//        });
-//
-//        await Promise.all(promises);
-//
-//        let travelTimes = [];
-//        if (value && latitude && longitude) {
-//            const travelTimesResponse = await axios.get(`https://us-central1-b-surf.cloudfunctions.net/calculateTravelTimes?lat=${latitude}&lng=${longitude}`);
-//            travelTimes = travelTimesResponse.data;
-//        }
-//
-//        const spotsWithStrongWindsArray = Object.keys(spotsWithStrongWinds).map(name => ({
-//            name,
-//            windStartTime: spotsWithStrongWinds[name].toISOString()
-//        }));
-//
-//        console.log('Spots with strong winds:', spotsWithStrongWindsArray);
-//
-//        const finalResponse = {
-//            spotsWithStrongWinds: spotsWithStrongWindsArray,
-//            travelTimes: travelTimes  // Include travel times in the response
-//        };
-//
-//        response.status(200).json(finalResponse);
-//    } catch (error) {
-//        console.error('Error:', error);
-//        response.status(500).send('An error occurred');
-//    }
-//});
+///////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-exports.scheduledAlerts = functions.pubsub.schedule('0 7 * * *') // have to be before 8am
-    //.timeZone('Asia/Jerusalem') // Israel timezone
+//send msg if wind at fav spot
+exports.scheduledAlerts = functions.pubsub.schedule('15 13 * * *').timeZone('Asia/Jerusalem')
     .onRun(async (context) => {
         try {
             const strongWindsResponse = await axios.get('https://us-central1-b-surf.cloudfunctions.net/getStrongWinds');
